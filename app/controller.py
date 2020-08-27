4b0d48a2841c1149236dd80f79351aa44280cebc
Robert Sharp <webmaster@sharpdesigndigital.com>
2020-08-26 20:28:07 -0700

    controler update

--- app/controller.py
+++ app/controller.py
@@ -7,6 +7,7 @@ from random import randrange
 from pandas import DataFrame
 from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_extraction.text import TfidfVectorizer
+from spacy.lang.en import English
 from app.model import DataModel
 
 
@@ -15,6 +16,18 @@ __all__ = ('PredictionBot',)
 
 class PredictionBot:
     """ NLP Bot for Cannabis Suggestion App """
+    
+    class Tokens:
+        nlp = English()
+
+        @classmethod
+        def tokenize(cls, document):
+            doc = cls.nlp(document)
+            return [
+                token.lemma_.strip() for token in doc
+                if not token.is_stop and not token.is_punct
+            ]
+
     data = DataModel()
     db = data.connect_db()
     df = data.read_csv()
@@ -23,14 +36,16 @@ class PredictionBot:
     name = df['Name']
     training = df['Description'] + ' ' + flavors + ' ' + effects + ' ' + name
     tfidf = TfidfVectorizer(
+        tokenizer=Tokens.tokenize,
         stop_words='english',
-        ngram_range=(1, 2),
-        max_features=8000,
+        ngram_range=(1, 3),
+        max_features=24000,
     )
     knn = NearestNeighbors(
         n_neighbors=1,
         n_jobs=-1,
-    ).fit(DataFrame(tfidf.fit_transform(training).todense()))
+    ).fit(pd.DataFrame(tfidf.fit_transform(training).todense()))
+
 
     def id_lookup(self, _id) -> dict:
         return next(self.db.find({'_id': int(_id)}))
