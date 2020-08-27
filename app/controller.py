4533c14dcfcc11dc2fa1a9e347990b2e41abe33d
BrokenShell <webmaster@sharpdesigndigital.com>
2020-08-26 20:30:02 -0700

    cleanup

--- app/controller.py
+++ app/controller.py
@@ -3,10 +3,11 @@ Controller Bot
 
 by Robert Sharp
 August 2020 """
-from Fortuna import random_below
+from random import randrange
 from pandas import DataFrame
 from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_extraction.text import TfidfVectorizer
+from spacy.lang.en import English
 from app.model import DataModel
 
 
@@ -15,6 +16,18 @@ __all__ = ('PredictionBot',)
 
 class PredictionBot:
     """ NLP Bot for Cannabis Suggestion App """
+
+    class Tokens:
+        nlp = English()
+
+        @classmethod
+        def tokenize(cls, document):
+            doc = cls.nlp(document)
+            return [
+                token.lemma_.strip() for token in doc
+                if not token.is_stop and not token.is_punct
+            ]
+
     data = DataModel()
     db = data.connect_db()
     df = data.read_csv()
@@ -23,9 +36,10 @@ class PredictionBot:
     name = df['Name']
     training = df['Description'] + ' ' + flavors + ' ' + effects + ' ' + name
     tfidf = TfidfVectorizer(
+        tokenizer=Tokens.tokenize,
         stop_words='english',
-        ngram_range=(1, 2),
-        max_features=8000,
+        ngram_range=(1, 3),
+        max_features=24000,
     )
     knn = NearestNeighbors(
         n_neighbors=1,
@@ -39,7 +53,7 @@ class PredictionBot:
         return next(self.db.find({'Name': name.title()}))
 
     def random(self) -> dict:
-        return self.id_lookup(random_below(2155))
+        return self.id_lookup(randrange(2155))
 
     def search(self, user_input: str) -> dict:
         vectors = self.tfidf.transform([user_input]).todense()
